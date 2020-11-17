a8ed1bbb50986b194e76839d810dd771afdec8a5
a-arshad <ali.stick.arshad@gmail.com>
2020-11-16 22:14:53 -0800

    Added functionality behind editmode

--- src/components/StoreView.js
+++ src/components/StoreView.js
@@ -4,17 +4,28 @@ import { NavLink } from 'react-router-dom';
 import ListingTable from './ListingTable'
 
 class StoreView extends Component {
+    constructor() {
+        super();
+        this.state = {isEditMode: false};
+    }
+
     render() {
         return (
             <div>
                 <h1>This is your Store!</h1>
                 <NavLink exact to="/" style={{paddingRight: 20}}>Home</NavLink>
                 <NavLink to="/consumer">ConsumerView</NavLink>
-                <ListingTable />
-                <Button onClick={() => alert("Edit store clicked")}>Edit store</Button>
+                <ListingTable isEditMode={this.state.isEditMode}/>
+                {this.state.isEditMode ? 
+                    <div>
+                        <Button onClick={() => this.setState({isEditMode: false})} style={{marginRight: 20}}>Cancel</Button>
+                        <Button onClick={() => this.setState({isEditMode: false})}>Confirm</Button>
+                    </div>
+                    : <Button onClick={() => this.setState({isEditMode: true})}>Edit store</Button>}
             </div>
         );
     }
+
 }
 
 export default StoreView;
