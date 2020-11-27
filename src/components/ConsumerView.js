87337370b0b5ac171e9b4dbf25b5e14b07323a8b
KeeganMcGinnis2 <keeganmcginnis219@gmail.com>
2020-11-26 17:54:48 -0800

    adding basic cart functions

--- src/components/ConsumerView.js
+++ src/components/ConsumerView.js
@@ -20,21 +20,81 @@ class ConsumerView extends Component {
         console.log(fetched_quantities);
         this.state = {
             listings: [
-                {name: 'Apple', description: 'YUM', price: '69', quantity: '5'},
-                {name: 'Banana', description: 'YUMMY', price: '5', quantity: '6'},
-                {name: 'Carrot', description: 'YumChampion', price: '2', quantity: '7'}
-            ]
+                {
+                  "inventoryId (S)": 1,
+                  "amount (N)": 100,
+                  "price (N)": 5,
+                  "priceUnit (S)": "per 5",
+                  "productDesc (S)": "Large sweet apples grown in BC.",
+                  "productName (S)": "Ambrosia Apples",
+                  "storeId (S)": 1
+                },
+                {
+                  "inventoryId (S)": 2,
+                  "amount (N)": 3,
+                  "price (N)": 2.99,
+                  "priceUnit (S)": "per 2 pounds",
+                  "productDesc (S)": "Thin sugary sweet potatoes used in Korean cuisine",
+                  "productName (S)": "Purple Sweet Potatoes",
+                  "storeId (S)": 1
+                },
+                {
+                  "inventoryId (S)": 3,
+                  "amount (N)": 30,
+                  "price (N)": 8.99,
+                  "priceUnit (S)": "per 5 pounds",
+                  "productDesc (S)": "Home grown apples that are very sour and satisfying.",
+                  "productName (S)": "Organic Granny Smith Apples",
+                  "storeId (S)": 1
+                }
+               ],
+            cart: []
         };
     }
 
+    addToCart(inventoryId, productName) {
+        var newCart = this.state.cart;
+        for (let i=0; i < newCart.length; i++) {
+            if (newCart[i].inventoryId === inventoryId) {
+                newCart[i].quantity += 1;
+                this.setState({
+                    cart: newCart
+                })
+                return;
+            }
+        }
+
+        var newCartItem = {'inventoryId': inventoryId, 'productName': productName, 'quantity': 1};
+        newCart.push(newCartItem);
+        this.setState({
+            cart: newCart
+        })
+    }
+
+    removeFromCart(productName) {
+        var newCart = this.state.cart;
+        for (let i=0; i < newCart.length; i++) {
+            if (newCart[i].productName === productName) {
+                newCart.splice(i, 1);
+                this.setState({
+                    cart: newCart
+                })
+            }
+        }
+    }
+
     render() {
+        var cartItems = [];
+        this.state.cart.forEach(element => cartItems.push(<h6 key={element}>{element.productName}: {element.quantity}&nbsp;
+            <button onClick={() => this.removeFromCart(element.productName)}>Remove from cart</button></h6>));
         return (
             <div>
-                <h1>This is your Consumer!</h1>
+                <h1>User {this.state.listings[0]["storeId (S)"]}'s Store</h1>
                 <NavLink exact to="/" style={{paddingRight: 20}}>Home</NavLink>
                 <NavLink to="/store">StoreView</NavLink>
-                <ListingTable isConsumer={true} listings={this.state.listings}/>
+                <ListingTable isConsumer={true} listings={this.state.listings} addToCart={this.addToCart.bind(this)}/>
                 <Button onClick={() => alert("Checkout clicked")}>Checkout</Button>
+                <div><h4>Cart:</h4> {cartItems}</div>
             </div>
         );
     }
--- src/components/ListingTable.js
+++ src/components/ListingTable.js
@@ -6,14 +6,16 @@ class ListingTable extends React.Component {
         let listings = [];
         this.props.listings.forEach(
             element => listings.push(<Listing
-                                            key={element.stockItemId} // TODO: change this to product id
-                                            name={element.stockItemId} 
-                                            description={element.stockItemId}
-                                            price={element.pricePer}
-                                            amount={element.amount}
+                                            key={element["inventoryId (S)"]} // TODO: change this to product id
+                                            id={element["inventoryId (S)"]}
+                                            name={element["productName (S)"]} 
+                                            description={element["productDesc (S)"]}
+                                            price={element["price (N)"] + " " + element["priceUnit (S)"]}
+                                            amount={element["amount (N)"]}
                                             isConsumer={this.props.isConsumer}
                                             isEditMode={this.props.isEditMode}
                                             updateStock={this.props.updateStock}
+                                            addToCart={this.props.addToCart}
                                     />));
 
         return (
