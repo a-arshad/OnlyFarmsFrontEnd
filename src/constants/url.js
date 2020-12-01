01c22dd511aeb83ba18459298d99a8a4132f46bf
a-arshad <ali.stick.arshad@gmail.com>
2020-11-30 23:05:13 -0800

    updated microservice links

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,2 @@
-export const INVENTORY_MS = "https://7ybj2whv04.execute-api.us-east-1.amazonaws.com/dev"
-export const CART_MS = "https://gr01no9opa.execute-api.us-east-1.amazonaws.com/dev"
+export const INVENTORY_MS = "https://gnmt2piyg7.execute-api.us-east-1.amazonaws.com/dev"
+export const CART_MS = "https://gxqweescff.execute-api.us-east-1.amazonaws.com/dev"
